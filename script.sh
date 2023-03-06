97041bcc960260cf218f63dd4db4886be97f03aa
Ashish <ashish@Beast-prod.3a1ktsmy5tvuzenlrx03dqoyvc.rx.internal.cloudapp.net>
2023-03-06 23:08:33 +0530

    script update

--- script.sh
+++ script.sh
@@ -1,15 +1,14 @@
-echo "Hello Ashis...this above is the payload recieved.h"
-echo" ------- "
-echo " Pulling github repo "
+echo " ------- Pulling github repo ------- "
 git pull
-" ---- "
 eval $(minikube docker-env)
-pwd
+#pwd
 # echo $PATH
-echo "----------Building images and applying manifests----------"
+echo "----------Building images----------"
 docker build -t challenge_ashpect ./teams/team1
+echo "----------Applying manifests---------"
 kubectl delete deployment challenge
 kubectl apply -f ./teams/team1/challenge.yaml
+echo "----------Deployment successfully created--------"
 
 
 

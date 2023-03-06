fccd8cad0a974766e04ea59a7e7e033289c7a68a
Ashish <ashish@Beast-prod.3a1ktsmy5tvuzenlrx03dqoyvc.rx.internal.cloudapp.net>
2023-03-06 18:58:33 +0530

    sdf

--- script.sh
+++ script.sh
@@ -1,9 +1,12 @@
-echo "hello"
-echo "ashish"
+echo "Hello Ashis...this above is the payload recieved.h"
+echo" ------- "
+echo " Pulling github repo "
 git pull
-eval $(minikube -p percy docker-env)
+" ---- "
+eval $(minikube docker-env)
 pwd
 # echo $PATH
+echo "----------Building images and applying manifests----------"
 docker build -t challenge_ashpect ./teams/team1
 kubectl delete deployment challenge
 kubectl apply -f ./teams/team1/challenge.yaml
